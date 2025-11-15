54693a66735ba36cb307a09324e01b6606668b02
raghu-kadali <raghu2.kadali@gmail.com>
2025-11-15 17:26:55 +0530

    kk

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -29,7 +29,7 @@ resource "google_compute_subnetwork" "subnet" {
 }
 
 # ---------------------------------------------------------
-# Firewall for HTTP (port 80)
+# Firewall for HTTP (port 80) - allow public + health checks
 # ---------------------------------------------------------
 resource "google_compute_firewall" "allow_http" {
   name    = "allow-http"
@@ -39,12 +39,31 @@ resource "google_compute_firewall" "allow_http" {
     protocol = "tcp"
     ports    = ["80"]
   }
-  source_ranges = ["0.0.0.0/0"]  # Allow all traffic (public)
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["php-server"]
+}
+
+resource "google_compute_firewall" "allow_gcp_healthchecks" {
+  name    = "allow-healthchecks"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = [
+    "130.211.0.0/22",
+    "35.191.0.0/16"
+  ]
+
   target_tags = ["php-server"]
 }
 
 # ---------------------------------------------------------
 # Instance Template (COS + Docker image from GAR)
+# Important: hostPort must be set so container port 80 is reachable on VM:80
 # ---------------------------------------------------------
 resource "google_compute_instance_template" "php_template" {
   name         = "php-instance-template"
@@ -52,23 +71,25 @@ resource "google_compute_instance_template" "php_template" {
   tags         = ["php-server"]
 
   disk {
-    auto_delete = true
-    boot        = true
+    auto_delete  = true
+    boot         = true
     source_image = "projects/cos-cloud/global/images/family/cos-stable"
-  }   # <-- THIS WAS MISSING
+  }
 
   network_interface {
     subnetwork = google_compute_subnetwork.subnet.id
   }
 
+  # Using GCE Container Declaration for COS
   metadata = {
     "gce-container-declaration" = <<EOF
 spec:
   containers:
   - name: php-app
     image: "${var.docker_image}"
-    stdin: false
-    tty: false
+    ports:
+      - containerPort: 80
+        hostPort: 80
   restartPolicy: Always
 EOF
   }
@@ -79,9 +100,8 @@ EOF
   }
 }
 
-
 # ---------------------------------------------------------
-# Managed Instance Group
+# Managed Instance Group (regional)
 # ---------------------------------------------------------
 resource "google_compute_region_instance_group_manager" "php_mig" {
   name               = "php-mig"
@@ -100,6 +120,13 @@ resource "google_compute_region_instance_group_manager" "php_mig" {
   }
 }
 
+# Wait for MIG to create the regional instance group resource id
+data "google_compute_region_instance_group" "php_mig_group" {
+  name   = google_compute_region_instance_group_manager.php_mig.instance_group
+  region = var.region
+  depends_on = [google_compute_region_instance_group_manager.php_mig]
+}
+
 # ---------------------------------------------------------
 # Health Check
 # ---------------------------------------------------------
@@ -107,7 +134,8 @@ resource "google_compute_health_check" "php_hc" {
   name = "php-health-check"
 
   http_health_check {
-    port = 80
+    port         = 80
+    request_path = "/"
   }
 
   timeout_sec        = 5
@@ -115,7 +143,9 @@ resource "google_compute_health_check" "php_hc" {
 }
 
 # ---------------------------------------------------------
-# Load Balancer
+# Backend Service (Global)
+# - Use balancing_mode and max_utilization to make backend stable
+# - Attach the regional instance group
 # ---------------------------------------------------------
 resource "google_compute_backend_service" "php_backend" {
   name        = "php-backend-service"
@@ -126,8 +156,20 @@ resource "google_compute_backend_service" "php_backend" {
   health_checks = [google_compute_health_check.php_hc.id]
 
   backend {
-    group = google_compute_region_instance_group_manager.php_mig.instance_group
+    group           = data.google_compute_region_instance_group.php_mig_group.self_link
+    balancing_mode  = "UTILIZATION"
+    max_utilization = 0.8
   }
+
+  # Optional: enable CDN or connection draining if desired
+  # enable_cdn = false
+}
+
+# ---------------------------------------------------------
+# URL Map, Proxy, Global IP, Forwarding Rule (Global HTTP LB)
+# ---------------------------------------------------------
+resource "google_compute_global_address" "php_lb_ip" {
+  name = "php-lb-ip"
 }
 
 resource "google_compute_url_map" "php_urlmap" {
@@ -141,8 +183,17 @@ resource "google_compute_target_http_proxy" "php_proxy" {
 }
 
 resource "google_compute_global_forwarding_rule" "php_forward_rule" {
-  name        = "php-forwarding-rule"
-  target      = google_compute_target_http_proxy.php_proxy.id
-  port_range  = "80"
+  name       = "php-forwarding-rule"
+  target     = google_compute_target_http_proxy.php_proxy.id
+  port_range = "80"
   ip_protocol = "TCP"
+  ip_address = google_compute_global_address.php_lb_ip.address
+}
+
+# ---------------------------------------------------------
+# (Optional) Output LB IP
+# ---------------------------------------------------------
+output "load_balancer_ip" {
+  value = google_compute_global_address.php_lb_ip.address
+  description = "Global external IP for the HTTP load balancer"
 }
