26ef29e6119bd5f9271a8ebca0f680271a3beb10
raghu-kadali <raghu2.kadali@gmail.com>
2025-11-15 17:30:36 +0530

    Update outputs.tf

--- project/terraform/outputs.tf
+++ project/terraform/outputs.tf
@@ -1,5 +1,4 @@
-output "load_balancer_ip" {
-  value = google_compute_global_forwarding_rule.php_forward_rule.ip_address
-  description = "Access the PHP application using this IP"
-}
-
+# output "load_balancer_ip" {
+#   value = google_compute_global_forwarding_rule.php_forward_rule.ip_address
+#   description = "Access the PHP application using this IP"
+# }
